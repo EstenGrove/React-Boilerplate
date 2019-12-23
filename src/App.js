093f526e8e464623128c2b115511dcd8bf1ea046
Steven G <estengrove99@gmail.com>
2019-12-23 08:45:03 -0700

    SUBJECT: Built out React Scaffold (project structure)
     On branch master
     Changes to be committed:
    	modified:   README.md
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    src/App.css
    	modified:   src/App.js
    	new file:   src/App.scss
    	new file:   src/components/Main.js
    	new file:   src/components/containers/CardLG.js
    	new file:   src/components/containers/CardMD.js
    	new file:   src/components/containers/CardSM.js
    	new file:   src/components/containers/Column.js
    	new file:   src/components/containers/ContainerLG.js
    	new file:   src/components/containers/Row.js
    	new file:   src/css/CardLG.module.scss
    	new file:   src/css/CardMD.module.scss
    	new file:   src/css/CardSM.module.scss
    	new file:   src/css/Column.module.scss
    	new file:   src/css/ContainerLG.module.scss
    	new file:   src/css/Main.module.scss
    	new file:   src/css/Row.module.scss
    	new file:   src/helpers/helpers.js
    	new file:   src/helpers/utils_containers.js
    	modified:   src/index.css
    	new file:   src/sass/_animations.scss
    	new file:   src/sass/_mixins.scss
    	new file:   src/sass/_variables.scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<h1>React Scaffold</h1>
+	<h4>Custom boilerplate for quick and concise project creation/generation.</h4>
     </div>
   );
 }
